8110838ad3990695d2f8aecb3615edbfaf7d1079
Mahesh Kumar <akashverma.mec18@itbhu.ac.in>
2020-07-08 20:59:49 +0530

    ver-1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Link, Switch, NavLink} from 'react-router-dom';
+import Home from './Home';
+import CountryPage from './CountryPage';
+import HistoryPage from './HistoryPage';
+import AboutPage from './AboutPage';
+import {Provider} from './Context';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' render={(routeProps) => <Home {...routeProps}/>} />
+          <Route exact path='/country' render={() => <CountryPage />} />
+          <Route exact path='/history' render={() => <HistoryPage />} />
+          <Route exact path='/about' render={() => <AboutPage />} />
+        </Switch>
+      </div>
+    </Provider>
   );
 }
+
+// function App(){
+//   return(
+//     <div>
+//       <Provider></Provider>
+//     </div>
+//   )
+// }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './image/cvirus-icon-lg.png';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="footer--icon">
+                <img src={logo} alt="logo"/>
+                <h1 className="logo-name">Covisafe</h1>
+            </div>
+            <div className="footer--content">
+                <div className="footer--content-links">
+                    <a href="#">Contact</a>
+                    <a href="#">Donate</a>
+                    <Link to="/about">About me</Link>
+                    <a href="/#top">Go to top</a>
+                </div>
+                <div className="footer--content-text">
+                    This website is built by <a href="http://www.linkedin.com/in/akash-verma-18135008" class="footer--link"> Akash Verma</a>. You can get
+                        the available code for this website on this <a href="https://github.com/Akash-vaerma-18135008/aayush-portfolio" class="footer--link"> Github repo</a>. You can use the code but do not claim it as your own.
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
